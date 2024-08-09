abb699109a2c06901da1f15f6119a229d50a6946
Dishant Yadav <dishantyadav599@gmail.com>
2024-08-10 02:43:14 +0530

    converted to a cli app

--- llm.py
+++ llm.py
@@ -5,6 +5,7 @@ from langchain_core.prompts import PromptTemplate
 
 from models import InvoiceDetails
 
+
 def process_invoice_text(text: str) -> InvoiceDetails:
 
     system_prompt = "Convert all numbers in the provided text that contain commas into a format without commas. For example, convert 6,790 to 6790. Ensure that all numbers with commas are correctly converted to their non-comma equivalents."
@@ -31,4 +32,3 @@ def process_invoice_text(text: str) -> InvoiceDetails:
     result = chain.invoke({"query": prompt})
 
     return result
-
